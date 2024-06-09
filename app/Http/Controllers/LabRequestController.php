58b47186a809b2f2d9a9c7f080c13bdf7d746987
ADorado <abramdorado18@gmail.com>
2024-06-09 17:19:05 +0800

    Fix Compile controllers and fix routing

--- app/Http/Controllers/LabRequestController.php
+++ app/Http/Controllers/LabRequestController.php
@@ -18,9 +18,10 @@ class LabRequestController extends Controller
         $labrequest = LabRequest::where('patient_number', $patient_number)
             ->orderBy('created_at', 'desc')
             ->first();
-        $request = json_decode($labrequest->request, true);
+        // $request = json_decode($labrequest->request, true);
 
-        return view('labrequest', compact('patient_number', 'labrequest', 'request'));
+        // return view('labrequest', compact('patient_number', 'labrequest', 'request'));
+        return view('labrequest', compact('patient_number', 'labrequest'));
     }
 
     public function store(Request $request, $patient_number) 
